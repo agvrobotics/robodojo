c4008048639ec595d266a21364776ae5593b134a
sierra-95 <michaelmachohi@gmail.com>
2025-09-20 23:59:22 +0300

    expected tree structure

--- setup.sh
+++ setup.sh
@@ -46,3 +46,5 @@ source install/setup.bash
 ros2 launch bringup nav2.launch.py
 
 ros2 launch nav2_bringup navigation_launch.py use_sim_time:=false
+
+ros2 launch nav2_bringup localization_launch.py map:=/home/sierra-95/Documents/robodojo/slam/bringup.yaml use_sim_time:=false
