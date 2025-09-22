4647224600d309b1b5ff215c60be0f909df0e29e
sierra-95 <michaelmachohi@gmail.com>
2025-09-22 14:34:27 +0300

    bringup naming package issue

--- slam/bringup/launch/odom.launch.py
+++ slam/bringup/launch/odom.launch.py
@@ -4,7 +4,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='odom_pub',
+            package='bringup',
             executable='odom_publisher',
             name='odom_publisher',
             output='screen'
