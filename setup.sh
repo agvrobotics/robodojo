c7d2fb40c38201a381c53056a6d29f783449382c
sierra-95 <michaelmachohi@gmail.com>
2025-09-24 23:00:20 +0300

    Did mapping with amcl

--- setup.sh
+++ setup.sh
@@ -28,8 +28,7 @@ ros2 run tf2_tools view_frames
 
 #### NAV2 #########################
 ros2 launch nav2_bringup localization_launch.py map:=/home/sierra-95/Documents/robodojo/slam/bringup.yaml use_sim_time:=false
-ros2 launch nav2_bringup navigation_launch.py params_file:=/home/sierra-95/Documents/robodojo/slam/bringup/params/nav2_params.yaml use_sim_time:=false
-
+ros2 launch nav2_bringup navigation_launch.py use_sim_time:=false map_subscribe_transient_local:=true
 
 #####LOADING A MAP#####
 #METHOD 1: using slamtoolbox Edit mapper_params_online_async.yaml
