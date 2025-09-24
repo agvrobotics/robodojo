bab27338ea736c1c3a1b2c13084a11473bc988e3
sierra-95 <michaelmachohi@gmail.com>
2025-09-24 19:39:40 +0300

    maps using yaml

--- slam/bringup/launch/slam.launch.py
+++ slam/bringup/launch/slam.launch.py
@@ -3,35 +3,14 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        # SLAM Toolbox Online Async Node
         Node(
             package='slam_toolbox',
             executable='async_slam_toolbox_node',
             name='slam_toolbox',
             output='screen',
-            parameters=[{
-                'use_sim_time': False,              # True if using Gazebo
-                'odom_frame': 'odom',
-                'map_frame': 'map',
-                'base_frame': 'base_link',
-                'scan_topic': 'scan',               # your lidar topic
-                'odom_topic': 'odom',               # your odom topic
-                'mode': 'mapping',                  # continuous mapping
-                'resolution': 0.003,                 # small cells → smooth map
-                'max_laser_range': 10.0,           
-
-                # Fast, frequent updates
-                'map_update_interval': 0.02,         # map integration frequency
-                'minimum_travel_distance': 0.03,    # update after 3 cm
-                'minimum_travel_heading': 0.02,     # update after 2°
-
-                # Scan matching improves smoothness
-                'use_scan_matching': True,
-                'use_scan_matching_odometry': True,
-
-                # Transform publishing
-                'transform_publish_period': 0.01,
-                'minimum_time_interval': 0.01
-            }]
+            parameters=[
+                '/home/sierra-95/Documents/robodojo/slam/bringup/params/mapper_params_online_async.yaml',
+                {'use_sim_time': False}
+            ]
         ),
     ])
