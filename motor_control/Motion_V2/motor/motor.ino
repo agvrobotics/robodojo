c752832d62b5436cdfc5f241de6244797a531b53
sierra-95 <michaelmachohi@gmail.com>
2025-10-04 15:12:08 +0300

    trying param file

--- motor_control/Motion_V2/motor/motor.ino
+++ motor_control/Motion_V2/motor/motor.ino
@@ -95,7 +95,7 @@ void loop() {
       }
       else if (strncmp(buffer, "TIP,", 4) == 0) {
         int action = atoi(buffer + 4);
-        if (action == 1) targetAngle = 50;
+        if (action == 1) targetAngle = 40;
         else if (action == 0) targetAngle = 0;
       }
     }
